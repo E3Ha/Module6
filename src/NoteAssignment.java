0e63c90c5db164b8f17cefd644f656b7314d0873
E3Ha <emma.harrison@talawanda.org>
2024-02-20 13:53:41 -0500

    2/20/2024 1:54

--- src/NoteAssignment.java
+++ src/NoteAssignment.java
@@ -1,5 +1,67 @@
-public class Main {
+import java.util.Scanner;
+
+public class NoteAssignment {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        double userInput = 0;
+        Scanner scan = new Scanner(System.in);
+        boolean done = false;
+        String trash = "";
+
+
+        do {
+
+            do {
+
+                System.out.println("Double: ");
+
+                if (scan.hasNextDouble()) {
+
+                    userInput = scan.nextDouble();
+                    done = true;
+
+                } else {
+
+                    trash = scan.nextLine();
+                    System.out.println("Incorrect Input: " + trash);
+
+
+                }
+
+
+            }
+            while (done == false);
+
+
+            //if statements
+
+            if (userInput > 0) {
+
+                System.out.println("Positive: " + userInput);
+
+            } else if (userInput < 0) {
+
+                System.out.println("Negative: " + userInput);
+
+            } else {
+
+                System.out.println("Zero: " + userInput);
+        
+            }
+            if (userInput > 1000000) {
+
+                System.out.println("Large");
+
+            } else if (Math.abs(userInput) < 1) {
+
+                System.out.println("Small");
+
+            }
+            done = false;
+        }
+        while (done == false);
+
     }
 }
